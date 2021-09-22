1f6abb825c12b829412fb70166a7a44a1dd8796b
Matheus Victor Alves da Silva <48606348+MatheusXDs@users.noreply.github.com>
2021-09-21 21:09:13 -0300

    Update lstm_melody.py
    Normalização do tempo da melodia

--- lstm_melody.py
+++ lstm_melody.py
@@ -9,6 +9,8 @@ import numpy
 import os
 import json
 
+ultima_pos = dict({"inicio":[],"fim":[]})
+maior_duracao = 0
 # carrega o modelo word2vec treinado
 def get_word2Vec_model(file_path):
     keyed_vectors = KeyedVectors.load(file_path)
@@ -16,7 +18,8 @@ def get_word2Vec_model(file_path):
     
 # retorna todas as letras e melodias da base de dados
 def get_all_lyrics_and_melody(midi_path_dir):
-
+    global ultima_pos, maior_duracao
+    
     data = dict({
         "lyrics": [],
         "melodies": []
@@ -30,16 +33,33 @@ def get_all_lyrics_and_melody(midi_path_dir):
 
             if file == "letra.json":
                 file_path = os.path.join(midi_folder_path, file)
-                with open(file_path, encoding='utf-8') as json_file:
+                with open(file_path) as json_file:
                     lyics_object = json.load(json_file)
                     lyrics_vec = ret_lyrics_vec(lyics_object)
                     data["lyrics"].append(lyrics_vec)
             if file == "melodia.json":
                 file_path = os.path.join(midi_folder_path, file)
-                with open(file_path, encoding='utf-8') as json_file:
+                with open(file_path) as json_file:
                     melody_object = json.load(json_file)
                     melody_vec = ret_melody_vec(melody_object)
                     data["melodies"].append(melody_vec)
+            # ultima_pos.append(data["melodies"][i]['fim'])
+        
+    # print(data["melodies"][0][0])
+    # print(data["melodies"][0][len(data["melodies"][0])-1])
+    for i in range(len(data["melodies"])):
+        # print(data["melodies"][i][len(data["melodies"][i])-1]["inicio"])
+        # print(data["melodies"][i][len(data["melodies"][i])-1]["fim"])
+        ultima_pos["inicio"].append(data["melodies"][i][len(data["melodies"][i])-1]["inicio"])
+        ultima_pos["fim"].append(data["melodies"][i][len(data["melodies"][i])-1]["fim"])
+        for f in range(len(data["melodies"][i])):
+            if data["melodies"][i][f]["duracao"] >= maior_duracao:
+                maior_duracao = data["melodies"][i][f]["duracao"]
+    print(maior_duracao)
+    # print(ultima_pos["inicio"])
+        
+    # print(data["melodies"][1][len(data["melodies"][0])-1])
+            
 
     return data
                 
@@ -57,19 +77,19 @@ def get_word_from_lyrics_vector(lyrics_vector):
     
     return words
 
-# retorna a matriz contendo todos os vetores de supervisão no formato [pitch, inicio, fim] de todas as musicas
+# retorna a matriz contendo todos os vetores de supervisÃ£o no formato [pitch, inicio, fim] de todas as musicas
 def get_all_supervised_vectors(data):
 
     supervised_vectors = []
 
     for i in range(len(data["melodies"])):
-        supervised_vector = get_supervised_vector(data["lyrics"][i], data["melodies"][i])
+        supervised_vector = get_supervised_vector(data["lyrics"][i], data["melodies"][i],i)
         for vector in supervised_vector:
             supervised_vectors.append(vector)
 
     return supervised_vectors
 
-# retorna a matriz contendo todos os vetores numéricos obtidos pelo word2vec de todas as letras de musicas
+# retorna a matriz contendo todos os vetores numÃ©ricos obtidos pelo word2vec de todas as letras de musicas
 def get_all_embedding_vectors(data, keyedVector):
     all_embedding_vectors = []
 
@@ -98,14 +118,14 @@ def get_lyrics_word_embedding(lyrics_vector, keyedVector):
     return lyrics_embedding_vector
 
 # retorna o vetor de dados supervisionados com o formato [pitch, tempo_inicial, tempo_final] das notas tocadas no mesmo tempo da letra
-def get_supervised_vector(lyrics_vector, melody_vector):
+def get_supervised_vector(lyrics_vector, melody_vector, ind):
     
     supervised_vectors = []
 
     for word in lyrics_vector:
         for melody in melody_vector:
             if word["tempo"] >= melody["inicio"] and word["tempo"] <= melody["fim"] or word["tempo"] <= melody["inicio"]:
-                melody_normalized = normalize_data(melody)
+                melody_normalized = normalize_data(melody, ind)
                 supervided_vector = [melody_normalized["pitch"],melody_normalized["inicio"],melody_normalized["fim"]]
                 supervised_vectors.append(supervided_vector)
                 break
@@ -123,15 +143,21 @@ def get_word_embedding_vector(lyrics_embedding_vector):
 
     return word_embedding_vector
 
-def normalize_data(melody):
-    pitch = melody["pitch"] = melody["pitch"]/88
+def normalize_data(melody, ind):
+    global ultima_pos, maior_duracao
+    
+    pitch = melody["pitch"] = melody["pitch"]/89
+    duracao = melody["duracao"]/ (maior_duracao + 1)
+    inicio = melody["inicio"]/ (ultima_pos["inicio"][ind] + 1) 
+    final = melody["fim"]/ (ultima_pos["fim"][ind] + 1)
+    print(inicio, final, duracao)
     return dict({
         "pitch": pitch,
-        "inicio": melody["inicio"],
-        "fim": melody["fim"]
+        "inicio": inicio,
+        "fim": final,
+        "duracao":duracao
     })
     
-
 def pre_process(embedding_vectors,supervised_vectors):
     
     embedding_vectors = numpy.array(embedding_vectors)
@@ -177,13 +203,10 @@ def lstm_model(output_units,n_timesteps, n_features,batch_size, embedding_vector
 
 
 def main():
-    keyed_vectors = get_word2Vec_model("/home/guimaj/Documentos/tcc/lyrics-vectors.kv") #path contendo o modelo word2vec exportado
-    data = get_all_lyrics_and_melody("/home/guimaj/Documentos/tcc/midi_teste") #path contendo as pastas dos midis extraidos
+    keyed_vectors = get_word2Vec_model("/home/lcsampaio/Documents/TCC/LSTM/w2v/lyrics-vectors.kv") #path contendo o modelo word2vec exportado
+    data = get_all_lyrics_and_melody("/home/lcsampaio/Documents/TCC/LSTM/Midis") #path contendo as pastas dos midis extraidos
     supervised_vectors = get_all_supervised_vectors(data)
     embedding_vectors  = get_all_embedding_vectors(data,keyed_vectors)
-    lstm_model(3,499,128,1, embedding_vectors, supervised_vectors)
+    lstm_model(3,607,100,1, embedding_vectors, supervised_vectors)
 
 main()
-
-
-
